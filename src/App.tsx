e5ac1637b495c9700760d8706b69907075272f28
robonexx <robertwagar@gmail.com>
2023-09-23 13:27:32 +0200

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import ExerciseList from './components/exerciseList/ExerciseList';
 import ExerciseBanner from './components/exercisebanner/ExerciseBanner';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <>
+      <Header>
+        
+      </Header>
       <main className='w-full min-h-screen flex flex-col items-center bg-slate-400 text-black box-border lg:w-2/3 lg:m-auto'>
         <h1 className='body-font font-black text-4xl'>Your daily routine</h1>
        <ExerciseBanner />
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import { motion } from 'framer-motion';
+
+interface HeaderProps {
+  children: ReactNode;
+}
+
+const Header: React.FC<HeaderProps> = ({ children }) => {
+  return (
+    <header className='relative w-full h-16 flex items-center justify-between'>
+      {children}
+      <motion.hr
+        className='absolute top-16 w-0 border-b border-white border-opacity-20 transition-all duration-500 ease-in-out'
+        initial={{ width: '0%' }}
+        animate={{ width: '100%' }}
+        transition={{ duration: 1.6, delay: 0.3, ease: 'easeInOut' }}
+      />
+    </header>
+  );
+};
+
+export default Header;
