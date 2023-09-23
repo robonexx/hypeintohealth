6919ecf9f19d066eef41ad14cba1b0af28f34dd5
robonexx <robertwagar@gmail.com>
2023-09-23 19:38:55 +0200

    fixed heading size

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <Nav />
       </Header>
       <main className='flex flex-col items-center bg-slate-400 text-black box-border lg:w-2/3 lg:mx-auto'>
-        <h1 className='mt-16 mb-4 py-2 bg-gradient-to-tr from-yellow-600 via-purple-800 to-orange-700 bg-clip-text text-transparent drop-shadow-xl font-extrabold body-font font-black text-xl md:text-2xl lg:text-4xl'>Hype Into Health</h1>
+        <h1 className='mt-16 mb-4 py-2 bg-gradient-to-tr from-yellow-600 via-purple-800 to-orange-700 bg-clip-text text-transparent drop-shadow-xl font-extrabold body-font font-black text-2xl md:text-4xl'>Hype Into Health</h1>
         <ExerciseBanner />
         <ExerciseList />
       </main>
