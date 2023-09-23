d483acb5e2e1cfabef8580a828ad7a66c4727dd0
robonexx <robertwagar@gmail.com>
2023-09-23 16:40:40 +0200

    changes to text

--- src/components/exercisebanner/ExerciseBanner.tsx
+++ src/components/exercisebanner/ExerciseBanner.tsx
@@ -6,12 +6,12 @@ const ExerciseBanner: React.FC = () => {
   return (
     <section className='container mx-auto px-6 p-10 lg:px-40'>
       <h2 className='text-4xl font-bold text-center text-gray-800 mb-40'>
-       Welcome to the training DOJO
+        Welcome to better helth
       </h2>
       <div className='flex items-center justify-between flex-wrap mb-40'>
         <div className='w-full md:w-1/2'>
           <h4 className='text-3xl text-gray-800 font-bold mb-3'>
-            Where to begin?
+            Your help online to start working out
           </h4>
           <p className='text-gray-600 mb-8'>
             Whether you're taking your first steps into the world of exercise or
@@ -19,6 +19,21 @@ const ExerciseBanner: React.FC = () => {
             counts. Starting to exercise is your gateway to a healthier, happier
             you. Embrace the momentum, and let each workout be a step toward
             your brighter, healthier future.
+          </p>
+        </div>
+        <div className='w-full md:w-2/6 p-8 md:p-4'>
+          <img src={IMG} alt='Monitoring' />
+        </div>
+      </div>
+      <div className='flex items-center justify-between flex-wrap mb-40'>
+        <div className='w-full md:w-1/2'>
+          <h4 className='text-3xl text-gray-800 font-bold mb-3'>
+            Where to begin?
+          </h4>
+          <p className='text-gray-600 mb-8'>
+            We recommend to just begin right where you stand. Are you at you
+            home? well we got the perfect set of exercises to get you started
+            And as you progress you can change the exercise catergory
           </p>
         </div>
         <div className='w-full md:w-2/6 p-8 md:p-4'>
