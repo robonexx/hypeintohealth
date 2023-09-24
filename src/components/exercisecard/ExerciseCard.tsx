190b84dc3a13886bd361f9d611c308a3b962d400
robonexx <robertwagar@gmail.com>
2023-09-24 11:00:56 +0200

    added icons to exericiseCards

--- src/components/exercisecard/ExerciseCard.tsx
+++ src/components/exercisecard/ExerciseCard.tsx
@@ -39,10 +39,10 @@ const ExerciseCard: React.FC<ExerciseCardProps> = ({ name, description }) => {
       <div className='flex items-center justify-between mb-2'>
         <h3 className='text-lg font-semibold'>{name}</h3>
         <button
-          className='text-blue-500 hover:text-blue-700 focus:outline-none'
+          className='text-blue-500 hover:text-blue-700 focus:outline-none text-2xl'
           onClick={toggleDescription}
         >
-          {showDescription ? 'Hide Description' : 'Show Description'}
+          {showDescription ? <FiArrowUpCircle /> : <FiArrowDownCircle />}
         </button>
       </div>
       {showDescription && <p className='text-gray-600'>{description}</p>}
