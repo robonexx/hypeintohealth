63554acc2ee4a5256527b7cdfe46f6b8b41f4442
robonexx <robertwagar@gmail.com>
2023-09-24 15:15:46 +0200

    fast fix on nav, set a border for a nice effect on navItem

--- src/components/nav/NavItem.tsx
+++ src/components/nav/NavItem.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 interface NavItemProps {
   path: string;
-    title: string;
-    key: number;
+  title: string;
+  key: number;
 }
 
 const NavItem: React.FC<NavItemProps> = ({ title, path }) => {
   return (
-    <li className='hover:border-b hover:border-gray-400 my-8 uppercase'>
+    <li className='group my-8 uppercase relative'>
       <a href={path}>{title}</a>
+      <div className='absolute bottom-0 left-0 w-0 border-b border-transparent group-hover:w-full group-hover:border-b group-hover:border-gray-400 transition-all duration-300'></div>
     </li>
   );
 };
