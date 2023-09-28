e2390b0470970a6d144a42393f32808a0c153552
robonexx <robertwagar@gmail.com>
2023-09-28 07:29:39 +0200

    input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,52 @@
+import { FC, ChangeEvent } from 'react';
+
+interface InputProps {
+  type: 'text' | 'number' | 'email' | 'password';
+  label: string;
+  name: string;
+  value: string | number;
+  placeholder: string;
+  error: boolean;
+  disabled?: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: FC<InputProps> = ({
+  type,
+  label,
+  value,
+  name,
+  placeholder,
+  error,
+  disabled,
+  onChange,
+}) => {
+  return (
+    <div className='mt-5'>
+      <label
+        htmlFor={label}
+        className='text-lg font-semibold w-60 bg-slate-400 text-slate-50 '
+      >
+        {label}
+      </label>
+      <input
+        className='text-base font-normal w-60 bg-slate-400 text-slate-50 rounded-lg border-2 border-current placeholder-[#667085]'
+        type={type}
+        id={label}
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={onChange}
+      >
+        {error && (
+          <p className='text-[#db4437] text-sm font-normal ml-3 mt-1'>
+            This field can't be empty
+          </p>
+        )}
+      </input>
+    </div>
+  );
+};
+
+export default Input;
