4a83f24a11ff560a298fc98636a45b9f63891cc5
robonexx <robertwagar@gmail.com>
2023-09-29 08:19:12 +0200

    done did it

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,12 @@
 const Contact = () => {
   return (
-    <main className='flex flex-col items-center bg-slate-400 text-black box-border lg:w-2/3 lg:mx-auto'>
-      <h1 className='mt-16 mb-4 py-2 bg-gradient-to-b from-orange-500 to-yellow-300 bg-clip-text text-transparent drop-shadow-xl font-extrabold body-font font-black text-3xl md:text-4xl lg:text-6xl'>
+    <main className='flex flex-col items-center bg-slate-400 text-black box-border lg:w-2/3 lg:mx-auto h-screen'>
+      <h1 className='mt-64 mb-4 py-2 bg-gradient-to-b from-orange-500 to-yellow-300 bg-clip-text text-transparent drop-shadow-xl font-extrabold body-font font-black text-3xl md:text-4xl lg:text-6xl'>
         Contact me
       </h1>
+      <h2 className='mt-16 mb-4 py-2 bg-gradient-to-r from-gray-700 via-gray-900 to-black bg-clip-text text-transparent drop-shadow-md font-extrabold body-font font-black text-xl md:text-2xl lg:text-3xl'>
+        robertwagar@gmail.com
+      </h2>
     </main>
   );
 };
