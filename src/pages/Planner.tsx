dc59eda5c8150c762c3bf6ede0981dfd99996485
robonexx <robertwagar@gmail.com>
2023-09-29 05:52:57 +0200

    cleaned planner page

--- src/pages/Planner.tsx
+++ src/pages/Planner.tsx
@@ -1,38 +1,8 @@
-import { FC, useState, FormEvent, ChangeEvent } from 'react';
+import { FC } from 'react';
 import ExercisePlanner from '../components/exercisePlanner/ExericisePlanner';
-/* import Input from '../components/Input/Input';
-import TextArea from '../components/textArea/TextArea'; */
 import CreateExercise from '../components/createExercise/CreateExercise';
 
 const Planner: FC = () => {
-/*   const [state, setState] = useState({
-    name: '',
-    description: '',
-  });
-  const [error, setError] = useState(false);
-
-  // handle on change
-  const handleOnChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const value = e.target.value;
-    setState({
-      ...state,
-      [e.target.name]: value,
-    });
-    console.log('onchange', state);
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!state.name.trim()) {
-      setError(true);
-    } else {
-      setError(false);
-      console.log(state.name, state.description)
-    }
-  };
- */
   return (
     <main className='flex flex-col items-center bg-slate-400 text-black box-border lg:w-2/3 lg:mx-auto'>
       <section>
@@ -42,39 +12,6 @@ const Planner: FC = () => {
         <ExercisePlanner />
         <CreateExercise />
       </section>
-      {/* <section>
-        <h1 className='mt-16 mb-4 py-2 bg-gradient-to-tr from-yellow-600 via-purple-800 to-orange-700 bg-clip-text text-transparent drop-shadow-xl font-extrabold body-font font-black text-2xl md:text-4xl'>
-          Create your own exercise
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
-        >
-          <Input
-            type='text'
-            label='Name'
-            value={state.name}
-            name='name'
-            error={error}
-            onChange={handleOnChange}
-            placeholder='Enter name of exercise'
-          />
-          <TextArea
-            label='Description'
-            value={state.description}
-            name='description'
-            error={error}
-            onChange={handleOnChange}
-            placeholder='Enter description'
-          />
-          <button
-            type='submit'
-            className='bg-indigo-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-8 my-4 w-1/3 md:w-32'
-          >
-            Submit
-          </button>
-        </form>
-      </section> */}
     </main>
   );
 };
